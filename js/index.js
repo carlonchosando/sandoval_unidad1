81ff6b8c83303ae55aad06bec6af9a1fceedf0e0
carlonchosando <cdscapitolio@gmail.com>
2022-09-04 14:30:06 -0300

    "conluida logica" 	modificados:     index.html 	modificados:     js/index.js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function seguroElegido() {
+    let seguroElegido = document.getElementById("tipoDeSeguro").value;
+    console.log(seguroElegido);
+    switch(seguroElegido) {
+        case "1": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 500,00"); break;
+        case "2": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 1000,00"); break;
+        case "3": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 1500,00"); break;
+    }
+}
