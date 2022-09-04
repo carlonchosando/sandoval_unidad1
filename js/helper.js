fb51699a590e9448ff7f16c43c3e8b74fe474e7b
carlonchosando <cdscapitolio@gmail.com>
2022-09-04 16:24:24 -0300

    "concluido el Practico-unidad-1" 	modificados:     css/index.css 	modificados:     index.html 	modificados:     js/helper.js 	modificados:     js/index.js

--- js/helper.js
+++ js/helper.js
@@ -5,6 +5,6 @@ var apellido = document.formData.apellido.value;
 var dni = document.getElementById("formData_dni").value;
 var email = document.getElementById("formData_email").value;
 var tel = document.getElementById("formData_telefono").value;
-console.log(nombre, apellido, dni, email, tel);
+console.log("nombre: " + nombre +", apellido: " + apellido + ", documento: " + dni + ", email: " + email + ", telefono: " + tel,);
 };
 
--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 function seguroElegido() {
     let seguroElegido = document.getElementById("tipoDeSeguro").value;    
     switch(seguroElegido) {
-        case "1": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 500,00"); break;
-        case "2": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 1000,00"); break;
-        case "3": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 1500,00"); break;
+        case "1": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 500,00 "); break;
+        case "2": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 1000,00 "); break;
+        case "3": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 1500,00 "); break;
     }
 }
