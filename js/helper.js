4e8ac39d55375be7ce67d5928139e6452c128ca4
carlonchosando <cdscapitolio@gmail.com>
2022-09-04 15:50:55 -0300

    "Concluido, falta css" 	nuevos archivos: 1.png 	modificados:     index.html 	modificados:     js/helper.js 	modificados:     js/index.js

---
+++ js/helper.js
@@ -0,0 +1,10 @@
+
+function capturaData() {
+var nombre = document.formData.nombre.value;
+var apellido = document.formData.apellido.value;
+var dni = document.getElementById("formData_dni").value;
+var email = document.getElementById("formData_email").value;
+var tel = document.getElementById("formData_telefono").value;
+console.log(nombre, apellido, dni, email, tel);
+};
+
--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 function seguroElegido() {
-    let seguroElegido = document.getElementById("tipoDeSeguro").value;
-    console.log(seguroElegido);
+    let seguroElegido = document.getElementById("tipoDeSeguro").value;    
     switch(seguroElegido) {
         case "1": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 500,00"); break;
         case "2": document.getElementById("mensaje").innerHTML=("El seguro Cuesta $ 1000,00"); break;
